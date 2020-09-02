28270cb9cb8bbc693f9644eb36e83ed835007d0d
jaguar <me4sun@gmail.com>
2020-09-02 16:44:22 -0700

    adding the upload to S3

--- backup_git.sh
+++ backup_git.sh
@@ -3,24 +3,36 @@
 
 # Add name of the repos. Could be read from a file also
 
-git_repos = "HelloWorld GoodbyeWorld"
-git_repos_path = "/work/code/HelloWorld"
+base_path="/home/sandeep/work"
+git_repos_path="${base_path}/code"
+curd=`dirname $0`
+uploadscript="${curd}/upload.sh"
 
-cur_dir = `dirname $0`
-backup_dir = "${git_repos_path}/git_backups"
+ts=`date +"%Y-%m-%d-%H-%M"`
+
+
+backup_dir="${base_path}/git_backups"
+filename="${backup_dir}/git-repo-backup.${ts}.tgz"
+logfile="${filename}.log"
 
 #Make the backup directory if does not exist
 
 mkdir -p ${backup_dir}
 
-
-for gr in ${git_repos}
-do
-
-	cp -prv $gr ${backup_dir}
-done
+[[ "$?" -ne 0 ]] && echo "Can not make the directory, Please check permissions" && exit 1
 
 
-echo "Backup completed"
+tar  -czvf $filename "${git_repos_path}"	 > $logfile  2>&1
 
 
+[[ "$?" -ne 0 ]] && echo "Could not create the backup file, Please check log" && exit 1
+
+
+echo "Calling the script to upload the target file to Blob STorage"
+
+ 
+source $uploadscript $filename
+
+
+
+[[ "$?" -ne 0 ]] && echo "Could not upload the backup file, Please check log" && exit 1
